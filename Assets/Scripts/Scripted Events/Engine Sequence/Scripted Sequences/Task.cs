58e1e5bfc4a875706ca94a50dd496e3357c8559f
Zach Daniel <zachary.ryan.daniel@gmail.com>
2018-02-09 15:48:06 -0600

    Adds the Engine Sequence prefab to the project (along with all scripts and animations necessary).
    Squashed commit:
    
    Moves Engine Sequence scripts to a separate folder. (+6 squashed commit)
    
    [d732fc32] improved formatting and style
    
    [8cf0a029] formatting
    
    [0e7d7858] this is the controller that used that animation
    
    [9d45e51b] this animation was for something else
    
    [e2b3a2ee] forgot a font lol
    
    [33c43a86] most of the stuff

---
+++ Assets/Scripts/Scripted Events/Engine Sequence/Scripted Sequences/Task.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Something the player has to do to advance the game
+/// </summary>
+public class Task : MonoBehaviour{
+
+    protected bool complete = false;
+    protected bool active = false;
+
+    [SerializeField]
+    protected string objectiveText;
+
+    public string ObjectiveText { get { return objectiveText; } protected set { objectiveText = value; } }
+
+    /// <summary>
+    /// true if the player has completed the task
+    /// </summary>
+    public bool Complete { get { return complete; } protected set { complete = value; } }
+    /// <summary>
+    /// true if this task can be completed next
+    /// </summary>
+    public bool Active { get { return active; } protected set { active = value; } }
+
+    //unused here, but made protected so that it can be overriden
+    protected virtual void Start()
+    {
+
+    }
+
+    /// <summary>
+    /// sets the task to be active
+    /// </summary>
+    public virtual void SetActive()
+    {
+        Active = true;
+    }
+
+    /// <summary>
+    /// sets the task to be inactive
+    /// </summary>
+    public virtual void SetInactive()
+    {
+        Active = false;
+    }
+
+    /// <summary>
+    /// called when player completes the task
+    /// </summary>
+    protected virtual void SetComplete()
+    {
+        Active = false;
+        Complete = true;
+        
+        if(OnTaskCompleted != null)
+        {
+            OnTaskCompleted.Invoke();
+        }  
+    }
+
+    /// <summary>
+    /// subscribe to this event to be notified when the task is completed
+    /// </summary>
+    public event System.Action OnTaskCompleted;
+}
