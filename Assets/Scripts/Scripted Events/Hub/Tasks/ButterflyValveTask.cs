c1230b5032c34e625a7eace1019d4b7be5cb4630
ROBERT-PC\Robert <robertsmbj@gmail.com>
2018-04-05 22:11:10 -0500

    Hub valve scripts

---
+++ Assets/Scripts/Scripted Events/Hub/Tasks/ButterflyValveTask.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This task is for turning the big valve in the engine room.
+/// The script is applied to a task object under "Tasks" in the hierarchy.
+/// </summary>
+
+public class ButterflyValveTask : Task
+{
+    [SerializeField, Tooltip("The GameObject controlling the valve interaction sequence. Once the big valve is turned, the task is complete.")]
+    private ValveInteractionSequence valveSequence;
+
+    private void OnEnable()
+    {
+        valveSequence.GeneratorBlewUp += SetComplete;
+    }
+    private void OnDisable()
+    {
+        valveSequence.GeneratorBlewUp -= SetComplete;
+    }
+
+    protected override void SetComplete()
+    {
+        base.SetComplete();
+    }
+}
---
+++ Assets/Scripts/Scripted Events/Hub/Tasks/ValveTask.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This task is for turning off all three of the valves in the engine room.
+/// The script is applied to a task object under "Tasks" in the hierarchy.
+/// </summary>
+
+public class ValveTask : Task
+{
+    [SerializeField, Tooltip("The GameObject controlling the valve interaction sequence. Once all three valves are turned, the task is complete.")]
+    private ValveInteractionSequence valveSequence;
+
+    private void OnEnable()
+    {
+        valveSequence.AllThreeValvesTurned += SetComplete;
+    }
+    private void OnDisable()
+    {
+        valveSequence.AllThreeValvesTurned -= SetComplete;
+    }
+
+    protected override void SetComplete()
+    {
+        base.SetComplete();
+    }
+}
