198ac093505685ceefead4c1e6106f09b0aafb89
Evan Sucher <evan.sucher@loop.colum.edu>
2018-04-12 19:58:40 -0500

    Fixed scripts based on PR comments

--- Assets/Scripts/Ray/GearRotation/RotateScript.cs
+++ Assets/Scripts/Ray/GearRotation/RotateScript.cs
@@ -1,64 +1,37 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class RotateScript : MonoBehaviour {
-
-    /// <summary>
-    /// This script is meant for constantly rotating gears, and constantly rotates the object it is attached to.
-    /// </summary>
+/// <summary>
+/// This script is meant for constantly rotating gears, and constantly rotates the object it is attached to.
+/// </summary>
+public class RotateScript : MonoBehaviour
+{
 
     #region Rotation Variables 
     [Tooltip("A vector that holds the x,y,z values for rotation speed")]
     [SerializeField]
-    public Vector3 RotationSpeed;
+    private Vector3 rotationSpeed;
 
     [Tooltip("Determines if object can rotate at all")]
     [SerializeField]
     public bool CanRotate;
-
-    [Tooltip("Determines if object can rotate around x axis")]
-    [SerializeField]
-    public bool CanRotateX;
-
-    [Tooltip("Determines if object can rotate around y axis")]
-    [SerializeField]
-    public bool CanRotateY;
-
-    [Tooltip("Determines if object can rotate around z axis")]
-    [SerializeField]
-    public bool CanRotateZ;
    
     //Space used when calling transform.Rotate
     private Space rotationSpace = Space.Self;
     #endregion
 
     // Update rotates the object, and if x,y,z are not rotatable, then it resets the respective variable to 0
-    void Update () {
-
+    void Update ()
+    {
         if (CanRotate)
         {
             Rotation();
         }
-
-        // Resets the corresponding variable to 0 if !CanRotate
-        if (!CanRotateX)
-        {
-            RotationSpeed.x = 0f;
-        }
-        if (!CanRotateY)
-        {
-            RotationSpeed.y = 0f;
-        }
-        if (!CanRotateZ)
-        {
-            RotationSpeed.z = 0f;
-        }
-    
     }
 
     //Rotates the object
     private void Rotation()
     {
-        this.transform.Rotate(RotationSpeed.x * Time.deltaTime, RotationSpeed.y * Time.deltaTime, RotationSpeed.z * Time.deltaTime, rotationSpace);
+        this.transform.Rotate(rotationSpeed.x * Time.deltaTime, rotationSpeed.y * Time.deltaTime, rotationSpeed.z * Time.deltaTime, rotationSpace);
     }
 }
--- Assets/Scripts/Ray/TextBoxTrigger/TextBoxTrigger.cs
+++ Assets/Scripts/Ray/TextBoxTrigger/TextBoxTrigger.cs
@@ -4,27 +4,24 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
+/// <summary>
+/// This script is used to print text to a canvas when the player enters a trigger box collider and remove 
+/// the text when the player exits the box collider. It is placed on an empty object with a box collider.
+/// </summary>
 public class TextBoxTrigger : MonoBehaviour
 {
-    /// <summary>
-    /// This script is used to print text to a canvas when the player enters a trigger box collider and remove 
-    /// the text when the player exits the box collider. It is placed on an empty object with a box collider.
-    /// </summary>
 
     [Tooltip("This is the canvas that the text is printed to")]
     [SerializeField]
-    public Canvas Canvas;
+    private Canvas canvas;
 
     [Tooltip("This is the text that gets printed to the canvas")]
     [SerializeField]
-    public Text TriggerText;
-   
-    // This is the player object that triggers the text
-    private CustomRigidbodyFPSController customRigidBodyFPSController;
+    private Text triggetText;
 
     void Start()
     {
-        TriggerText.enabled = false;
+        triggetText.enabled = false;
     }
 
     // Enables Canvas and Text when player enters trigger
@@ -32,8 +29,8 @@ public class TextBoxTrigger : MonoBehaviour
     {
         if(other.gameObject.tag == "Player")
         {
-            Canvas.enabled = true;
-            TriggerText.enabled = true;      
+            canvas.enabled = true;
+            triggetText.enabled = true;      
         }
     }
 
@@ -42,9 +39,9 @@ public class TextBoxTrigger : MonoBehaviour
     {
         if (other.gameObject.tag == "Player")
         {
-            Canvas.enabled = false;
-            TriggerText.enabled = false;
-            TriggerText.text = "";
+            canvas.enabled = false;
+            triggetText.enabled = false;
+            triggetText.text = "";
         }
         this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + 10, this.transform.position.z);
     }
