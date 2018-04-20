8ed4e946be625095c45622216aa62b2ca7efc561
Matvey_Doronichev <edoronichev@gmail.com>
2018-04-20 16:49:31 -0500

    orgonized and lit coments

--- Assets/Scripts/Norma/NormaLightFlikker/Norma_Light_flikking.cs
+++ Assets/Scripts/Norma/NormaLightFlikker/Norma_Light_flikking.cs
@@ -7,18 +7,9 @@ public class Norma_Light_flikking : MonoBehaviour {
     [SerializeField] 
     private Light fire;
 
-        
-
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void FixedUpdate () {
-
-        fire.intensity = Mathf.Lerp(fire.intensity, Random.Range(0.5f, 1.3f),0.25f);
-
-
+	void FixedUpdate ()
+    {
+        //barrell light flickers like real fire you would not belive it !
+        fire.intensity = Mathf.Lerp(fire.intensity, Random.Range(0.5f, 1.3f),0.25f); 
     }
 }
