590d760d9770b42dc6b1301bda1473165e682f89
Robert Bjornsson <robertsmbj@gmail.com>
2018-04-20 14:58:50 -0500

    Adds the alignment puzzle for the Ray level.

---
+++ Assets/Scripts/Ray/AlignmentPuzzle/ActivateObjects.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This script goes on any object that can be interacted with.
+/// It disables any number of gameobjects at the start, and
+/// enables them again when t is interacted with.
+/// </summary>
+public class ActivateObjects : MonoBehaviour, IInteractable
+{
+    [SerializeField]
+    [Tooltip("The GameObjects that should be enabled when the player interacts with this object.")]
+    private GameObject[] objectsToActivate;
+
+
+    /// <summary>
+    /// Disables all GameObjects as specified in the editor.
+    /// </summary>
+    protected virtual void Start()
+    {
+        // Start by disabling all of the objects.
+        foreach (GameObject gameObject in objectsToActivate)
+        {
+            gameObject.SetActive(false);
+        }
+    }
+
+    /// <summary>
+    /// Enables all GameObjects as specified in the editor.
+    /// </summary>
+    /// <param name="agentInteracting"></param>
+    public virtual void Interact(GameObject agentInteracting)
+    {
+        // Enable all of the objects
+        foreach(GameObject gameObject in objectsToActivate)
+        {
+            gameObject.SetActive(true);
+        }
+    }
+}
---
+++ Assets/Scripts/Ray/AlignmentPuzzle/RingPuzzle.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class goes on any object in the Alignment Puzzle prefab.
+/// It holds a reference to the rings that are a part of the puzzle.
+/// Its purpose is to broadcast an event when the puzzle has been completed.
+/// </summary>
+public class RingPuzzle : MonoBehaviour
+{
+    /// <summary>
+    /// Invoked when the puzzle is solved.
+    /// </summary>
+    public static event Action PuzzleUnlocked;
+    /// <summary>
+    /// Invoked when the puzzle is unsolved.
+    /// </summary>
+    public static event Action PuzzleLocked;
+
+    [SerializeField]
+    [Tooltip("When all of these rings are in the correct position, the puzzle is completed.")]
+    private Ring[] ringsToUnlock;
+
+    // Used to determine whether all of the rings are in the correct position.
+    private bool puzzleComplete = false;
+    // We only want to sent the event once.
+    private bool hasSentLockedEvent = false;
+
+    /// <summary>
+    /// Subscribe to each ring's state changed event.
+    /// </summary>
+    private void OnEnable()
+    {
+        foreach(Ring ring in ringsToUnlock)
+        {
+            ring.RingStateChanged += OnRingStateChanged;
+        }
+    }
+    /// <summary>
+    /// Unsubscribe from each ring's state changed event.
+    /// </summary>
+    private void OnDisable()
+    {
+        foreach(Ring ring in ringsToUnlock)
+        {
+            ring.RingStateChanged -= OnRingStateChanged;
+        }
+    }
+
+    /// <summary>
+    /// Notify subscribers if the puzzle has been solved or if it was just
+    /// unsolved.
+    /// </summary>
+    private void OnRingStateChanged()
+    {
+        puzzleComplete = CheckIfShouldGrantAccess();
+        // Only sent the moment the puzzle has been completed.
+        if (puzzleComplete)
+        {
+            PuzzleUnlocked.Invoke();
+            hasSentLockedEvent = false;
+        }
+        // Only sent the moment the puzzle stops being completed.
+        else if (hasSentLockedEvent == false)
+        {
+            PuzzleLocked.Invoke();
+            hasSentLockedEvent = true;
+        }
+    }
+
+    /// <summary>
+    /// If all of the rings are in the correct position, the puzzle is completed.
+    /// If even one ring is in the wrong position, the puzzle is not complete.
+    /// </summary>
+    /// <returns></returns>
+    private bool CheckIfShouldGrantAccess()
+    {
+        foreach (Ring ring in ringsToUnlock)
+        {
+            if (ring.IsRotationCorrect == false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+}
