2c1a2d32b7cfbe5058fb5736cb236ed9d57b94d8
Matvey_Doronichev <edoronichev@gmail.com>
2018-04-20 16:44:09 -0500

    worked on norma level done because that's what i do

---
+++ Assets/Scripts/Norma/NormaLightFlikker/Norma_Light_flikking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Norma_Light_flikking : MonoBehaviour {
+
+    [SerializeField] 
+    private Light fire;
+
+        
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+
+        fire.intensity = Mathf.Lerp(fire.intensity, Random.Range(0.5f, 1.3f),0.25f);
+
+
+    }
+}
