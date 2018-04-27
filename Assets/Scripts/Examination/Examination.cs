fa21fb604e07b0a029668deec5078cdadaf03575
Daniel.Caneda@iam.local <Daniel.Caneda@iam.local>
2018-04-27 13:46:29 -0500

    privatized variable

--- Assets/Scripts/Examination/Examination.cs
+++ Assets/Scripts/Examination/Examination.cs
@@ -98,7 +98,7 @@ public class Examination : MonoBehaviour, IInteractable
 
 
 	//static variable that detects whether ANY object is currently being inspected or not. Applies to all the object with same script.
-	static bool currentlyInspecting = false;
+	private static bool currentlyInspecting = false;
 
     #endregion
 
