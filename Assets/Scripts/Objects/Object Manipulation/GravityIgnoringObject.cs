6cbf042b09cf05d83a7d2a878951a0319a999491
ShoobiePower <nathan.choquette@loop.colum.edu>
2018-05-01 16:41:42 -0500

    A script to keep objects in place for the zero gravity at the start of a scene. You would attach this to objects that you would not want moving ( such as a stack of boxes).

---
+++ Assets/Scripts/Objects/Object Manipulation/GravityIgnoringObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+///  A simple script to attach to any object you would not want to move due to
+///  initial zero gravity of a scene swap. Attach this class to objects you want to stay in
+///  place for the zero gravity, but would want to move later ( example, the boxes in the ray level are stacked in a specific manner that
+///  may become disrupted).
+/// </summary>
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class GravityIgnoringObject : MonoBehaviour
+{
+
+    
+    private Rigidbody thisObjectsRigidbody;
+
+    private void Start()
+    {
+        thisObjectsRigidbody = this.GetComponent<Rigidbody>();
+    }
+    void Update ()
+    {
+        CheckForGravityChange();
+	}
+
+    private void CheckForGravityChange()
+    {
+        if (Physics.gravity == Vector3.zero)
+        {
+            thisObjectsRigidbody.isKinematic = true;
+        }
+
+        else
+        {
+            thisObjectsRigidbody.isKinematic = false;
+        }
+    }
+}
