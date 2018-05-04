70fc128c2e8594e70ee9cd986a5a5f5034dc004e
danewheaton <wheaton.dane@gmail.com>
2018-05-04 08:44:53 -0500

    fixed a couple minor things

--- Assets/Scripts/Scripted Events/Ending/PlayerEndingBehaviors.cs
+++ Assets/Scripts/Scripted Events/Ending/PlayerEndingBehaviors.cs
@@ -15,6 +15,9 @@ public class PlayerEndingBehaviors : MonoBehaviour
     [SerializeField, Tooltip("The main camera's glitch effect component.")]
     private GlitchEffect glitchEffect;
 
+    [SerializeField, Tooltip("How many times slower the player should move at half-power.")]
+    private float halfChargeSpeed = 4;
+
     private void OnEnable()
     {
         PlayerPowerable.TransferredPartOfPowerReserve += OnPowerDrained;
@@ -29,9 +32,9 @@ public class PlayerEndingBehaviors : MonoBehaviour
     /// </summary>
     private void OnPowerDrained()
     {
-        movementScript.movementSettings.ForwardSpeed /= 2;
-        movementScript.movementSettings.BackwardSpeed /= 2;
-        movementScript.movementSettings.StrafeSpeed /= 2;
+        movementScript.movementSettings.ForwardSpeed /= halfChargeSpeed;
+        movementScript.movementSettings.BackwardSpeed /= halfChargeSpeed;
+        movementScript.movementSettings.StrafeSpeed /= halfChargeSpeed;
 
         glitchEffect.enabled = true;
         glitchEffect._DispIntensity = .05f;
@@ -42,9 +45,9 @@ public class PlayerEndingBehaviors : MonoBehaviour
     /// </summary>
     private void OnPowerRestored()
     {
-        movementScript.movementSettings.ForwardSpeed *= 2;
-        movementScript.movementSettings.BackwardSpeed *= 2;
-        movementScript.movementSettings.StrafeSpeed *= 2;
+        movementScript.movementSettings.ForwardSpeed *= halfChargeSpeed;
+        movementScript.movementSettings.BackwardSpeed *= halfChargeSpeed;
+        movementScript.movementSettings.StrafeSpeed *= halfChargeSpeed;
 
         glitchEffect.enabled = false;
         glitchEffect._DispIntensity = 0;
