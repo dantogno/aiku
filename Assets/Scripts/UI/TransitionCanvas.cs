f52a64dc9a4f1715dc90813d7f1915de681f804f
ROBERT-PC\Robert <robertsmbj@gmail.com>
2018-05-06 00:53:37 -0500

    Adds prefab for a loading screen when switching levels.

---
+++ Assets/Scripts/UI/TransitionCanvas.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransitionCanvas : MonoBehaviour
+{
+    private TransitionCanvas instance;
+
+    private void Awake()
+    {
+        this.gameObject.SetActive(false);
+        bool exists = CheckForSingleton();
+        if(exists)
+        {
+            return;
+        }
+        SceneTransition.SceneChangeStarted += EnableThis;
+        SceneTransition.SceneChangeFinished += DisableThis;
+    }
+
+    private bool CheckForSingleton()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(this);
+            return false;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+            return true;
+        }
+    }
+
+    //private void Update()
+    //{
+    //    //Logic for setting alpha value of text.
+    //}
+
+    private void EnableThis()
+    {
+        this.gameObject.SetActive(true);
+    }
+
+    private void DisableThis()
+    {
+        this.gameObject.SetActive(false);
+    }
+}
