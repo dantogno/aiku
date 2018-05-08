c1bc6a03e4634be4aec7b7af8a2e2f11043eb009
dane <wheaton.dane@gmail.com>
2018-05-08 12:28:03 -0500

    add visual and audio feedback when the player uses the hologenerator puzzle, fix Ray level so that game is finishable again

---
+++ Assets/Scripts/Audio/MakeSoundWhenRingSpins.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class plays a sound when the player spins the hologenerator rings.
+/// The script is applied to a prefab which should be added into the hologenerator asset's hierarchy.
+/// </summary>
+
+[RequireComponent(typeof(AudioSource))]
+public class MakeSoundWhenRingSpins : MonoBehaviour
+{
+    private AudioSource myAudioSource;
+
+    private void OnEnable()
+    {
+        RotateRings.RotatedRings += PlaySound;
+    }
+    private void OnDisable()
+    {
+        RotateRings.RotatedRings -= PlaySound;
+    }
+
+    /// <summary>
+    /// Play a sound when a ring is rotated.
+    /// </summary>
+    public void PlaySound()
+    {
+        myAudioSource = GetComponent<AudioSource>();
+
+        if (myAudioSource != null) myAudioSource.Play();
+    }
+}
---
+++ Assets/Scripts/Ray/AlignmentPuzzle/HubHologeneratorFX.cs
@@ -0,0 +1,125 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class adds some player feedback to the hologenerator, in the form of visual and audio effects.
+/// The script is applied to the Hub Hologenerator GameObject.
+/// </summary>
+
+public class HubHologeneratorFX : MonoBehaviour
+{
+    [SerializeField, Tooltip("The shutdown button on the hologenerator.")]
+    private RingPuzzleButton button;
+
+    [SerializeField, Tooltip("The AudioSource component which plays an error sound.")]
+    private AudioSource errorSource;
+
+    [SerializeField, Tooltip("The AudioSource component which plays a shutdown sound.")]
+    private AudioSource shutdownSource;
+
+    [SerializeField, Tooltip("The Renderer components whose emissives we want to disable on shutdown.")]
+    private Renderer[] renderersToDisable;
+
+    [SerializeField, Tooltip("The GameObjects we want to disable on shutdown.")]
+    private GameObject[] gameObjectsToDeactivate;
+
+    [SerializeField, Tooltip("The interactable scripts we want to disable on shutdown.")]
+    private MonoBehaviour[] interactablesToDisable;
+
+    [SerializeField, Tooltip("The color the emissives should flash when there is an error.")]
+    private Color errorColor = Color.magenta;
+
+    [SerializeField, Tooltip("The color the emissives should flash when there is an error.")]
+    private Color errorAlbedoColor = Color.magenta;
+
+    [SerializeField, Tooltip("How long the error color should last, in seconds.")]
+    private float errorWaitTime = 1;
+
+    [SerializeField, Tooltip("How long the shutdown should last, in seconds.")]
+    private float shutdownWaitTime = 1;
+
+    private void OnEnable()
+    {
+        HubRotateRings.TriedToSpinRings += OnTriedToSpinRings;
+        button.ButtonPressed += OnButtonPressed;
+    }
+    private void OnDisable()
+    {
+        HubRotateRings.TriedToSpinRings -= OnTriedToSpinRings;
+        button.ButtonPressed -= OnButtonPressed;
+    }
+
+    /// <summary>
+    /// When the player tries to spin the rings before the puzzle is activated, turn the emissives red and play an error noise.
+    /// </summary>
+    private void OnTriedToSpinRings()
+    {
+        foreach (Renderer r in renderersToDisable) StartCoroutine(ShowErrorEmissionColor(r));
+
+        if (errorSource != null) errorSource.Play();
+    }
+
+    /// <summary>
+    /// When the player presses the shutdown button: play a sound, turn off emissives, and deacivate GameObjects.
+    /// </summary>
+    private void OnButtonPressed()
+    {
+        foreach (Renderer r in renderersToDisable) StartCoroutine(TurnOffEmissionAndLowerShutdownPitch(r));
+
+        foreach (GameObject g in gameObjectsToDeactivate) g.SetActive(false);
+
+        foreach (MonoBehaviour m in interactablesToDisable) m.enabled = false;
+
+        shutdownSource.Play();
+    }
+
+    /// <summary>
+    /// Turn emissives red, and then back to original color to show that the player shouldn't be using the hologenerator .
+    /// </summary>
+    private IEnumerator ShowErrorEmissionColor(Renderer r)
+    {
+        Color originalColor = r.material.GetColor("_EmissionColor"),
+            originalAlbedoColor = r.material.color;
+
+        // Change to error emissive color.
+        r.material.SetColor("_EmissionColor", errorColor);
+
+        // Change to error color.
+        //r.material.color = errorAlbedoColor;
+
+        // Wait.
+        yield return new WaitForSeconds(errorWaitTime);
+
+        // Change back to original emissive color.
+        r.material.SetColor("_EmissionColor", originalColor);
+
+        // Change back to original color.
+        //r.material.color = originalAlbedoColor;
+    }
+
+    /// <summary>
+    /// Turn off emission, to show that the player has deactivated the bots. Also lower the pitch of the shutdown AudioSource.
+    /// </summary>
+    private IEnumerator TurnOffEmissionAndLowerShutdownPitch(Renderer r)
+    {
+        Color originalColor = r.material.GetColor("_EmissionColor"),
+                currentColor = originalColor;
+
+        shutdownSource.pitch = .5f;
+
+        float elapsedTime = 0;
+        while (elapsedTime < shutdownWaitTime)
+        {
+            currentColor = Color.Lerp(originalColor, Color.black, elapsedTime / shutdownWaitTime);
+            r.material.SetColor("_EmissionColor", currentColor);
+            shutdownSource.pitch = Mathf.Lerp(.5f, 0, elapsedTime / shutdownWaitTime);
+
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+
+        r.material.SetColor("_EmissionColor", Color.black);
+        shutdownSource.pitch = 0;
+    }
+}
