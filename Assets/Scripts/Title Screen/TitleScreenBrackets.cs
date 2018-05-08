c6b677c0797f38d65d682cd26d91cda628d1f985
Zachary.Daniel <Zachary.Daniel@loop.colum.edu>
2018-05-07 22:18:42 -0500

    adds TitleScreen scene and relevant scripts, changes build settings to load TitleScreen first

---
+++ Assets/Scripts/Title Screen/TitleScreenBrackets.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Simulates the brackets on the title screen UI
+/// </summary>
+public class TitleScreenBrackets : MonoBehaviour {
+
+    [SerializeField]
+    [Tooltip("RectTransform of the panel the brackets are anchored in")]
+    private RectTransform bracketArea;
+
+    [SerializeField]
+    [Tooltip("Rect the brackets return to when nothing is selected")]
+    private Rect defaultBracketArea;
+
+    [SerializeField]
+    [Tooltip("Bracket movement settings")]
+    private float bracketMoveTime = 0.1f, bracketMaxSpeed = 1000f;
+
+    /// <summary>
+    /// Used by SmoothDamp to track how quickly the brackets' anchors are moving each frame
+    /// </summary>
+    private Vector2 bracketAnchorMinVelocity, bracketAnchorMaxVelocity;
+
+    /// <summary>
+    /// The target rect we SmoothDamp towards
+    /// </summary>
+    private Rect targetArea;
+
+    /// <summary>
+    /// Move the brackets so they surround this UI element
+    /// </summary>
+    /// <param name="selection">The element to surround</param>
+    public void Select(RectTransform selection)
+    {
+        if(selection != null)
+        {
+            float x, y, width, height;
+
+            //convert from screen coords to normalized positions
+            x = selection.anchoredPosition.x / Screen.width;
+            y = selection.anchoredPosition.y / Screen.height;
+
+            width = selection.rect.width / Screen.width;
+            height = selection.rect.height / Screen.height;
+
+            //set the target
+            targetArea = new Rect(x, y, width, height);
+        }
+        else //return to normal
+        {
+            targetArea = defaultBracketArea;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+        targetArea = defaultBracketArea;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DrawBrackets(targetArea);
+	}
+
+    /// <summary>
+    /// Performs SmoothDamp to smoothly move the brackets
+    /// </summary>
+    /// <param name="rect">target to surround</param>
+    private void DrawBrackets(Rect rect)
+    {
+        bracketArea.anchorMin = Vector2.SmoothDamp(bracketArea.anchorMin, new Vector2(rect.min.x, rect.min.y), ref bracketAnchorMinVelocity, bracketMoveTime, bracketMaxSpeed, Time.deltaTime);
+
+        bracketArea.anchorMax = Vector2.SmoothDamp(bracketArea.anchorMax, new Vector2(rect.max.x, rect.max.y), ref bracketAnchorMaxVelocity, bracketMoveTime, bracketMaxSpeed, Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Title Screen/TitleScreenManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Handles miscellaneous title screen needs, like ensuring the lighting is correct and starting or quitting the game
+/// </summary>
+public class TitleScreenManager : MonoBehaviour {
+
+    /// <summary>
+    /// Do the cryochambers need to be powered off?
+    /// </summary>
+    private bool shouldPowerOff = true;
+
+    /// <summary>
+    /// Are we loading the opening scene?
+    /// </summary>
+    private bool loadingGame = false;
+
+    [SerializeField]
+    [Tooltip("The large black panel used for fading in and out of the title screen")]
+    private Image loadingScreen;
+
+    /// <summary>
+    /// Loading operation for the opening scene so we can check on the progress
+    /// </summary>
+    private AsyncOperation loadingOperation = null;
+
+    private void LateUpdate()
+    {
+        //Doing this in LateUpdate so that it happens after the power is all set up
+        if (shouldPowerOff)
+        {
+            //Turn off the cryochambers so that they have the magenta light over them
+            foreach (Cryochamber c in FindObjectsOfType<Cryochamber>())
+            {
+                c.PowerOff();
+            }
+
+            shouldPowerOff = false;
+        }
+    }
+
+    private void Update()
+    {
+        //if loading the opening scene is taking a while, fade slowly so the player can tell something is going on
+        if (loadingGame)
+        {
+            Color c = Color.clear;
+            c.a = loadingOperation.progress;
+        }
+    }
+
+    /// <summary>
+    /// Go to the OpeningScene
+    /// </summary>
+    public void StartGame()
+    {
+        loadingGame = true;
+        loadingOperation = UnityEngine.SceneManagement.SceneManager.LoadSceneAsync("OpeningScene");
+    }
+
+    /// <summary>
+    /// Close the application
+    /// </summary>
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
