b02bdf5dd4410dac17887942386bfbc7b9065622
ROBERT-PC\Robert <robertsmbj@gmail.com>
2018-05-09 02:27:26 -0500

    Adds the prefab to Hub for testing. Also comments and cleans up the TransitionCanvas script.

--- Assets/Scripts/UI/TransitionCanvas.cs
+++ Assets/Scripts/UI/TransitionCanvas.cs
@@ -2,22 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This script is a singleton. It goes on the canvas that is enabled
+/// when the player enters a level from the Hub.
+/// </summary>
 public class TransitionCanvas : MonoBehaviour
 {
     private TransitionCanvas instance;
 
     private void Awake()
     {
+        // This starts inactive.
         this.gameObject.SetActive(false);
+        // Make sure this object is a singleton
         bool exists = CheckForSingleton();
         if(exists)
         {
+            // If it is a singleton, we don't want to subscribe.
             return;
         }
+        // Subscribe to scene transitions.
         SceneTransition.SceneChangeStarted += EnableThis;
         SceneTransition.SceneChangeFinished += DisableThis;
     }
 
+    /// <summary>
+    /// Ensures that this object is a singleton.
+    /// </summary>
+    /// <returns></returns>
     private bool CheckForSingleton()
     {
         if (instance == null)
@@ -33,11 +45,9 @@ public class TransitionCanvas : MonoBehaviour
         }
     }
 
-    //private void Update()
-    //{
-    //    //Logic for setting alpha value of text.
-    //}
-
+    /// <summary>
+    /// Enables the canvas if the player is loading a level from the Hub.
+    /// </summary>
     private void EnableThis()
     {
         if(SceneTransition.CurrentScene == "Hub")
@@ -46,6 +56,9 @@ public class TransitionCanvas : MonoBehaviour
         }
     }
 
+    /// <summary>
+    /// Disables the canvas.
+    /// </summary>
     private void DisableThis()
     {
         this.gameObject.SetActive(false);
