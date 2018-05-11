0b7ae412daf14ae83cbf4b14e1a14976ec94c0df
M7710-04\Student <wheaton.dane@gmail.com>
2018-05-11 10:44:42 -0500

    ray bugfixes

---
+++ Assets/Scripts/Ray/Elevator/DontAllowRoombaToRideTheElevator.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Apply this to the up elevator button.
+/// </summary>
+
+public class DontAllowRoombaToRideTheElevator : MonoBehaviour
+{
+    private RayElevatorButtonScript upButton;
+
+    private void Awake()
+    {
+        upButton = GetComponent<RayElevatorButtonScript>();
+        upButton.enabled = false;
+    }
+
+    private void OnEnable()
+    {
+        RayDoorTrigger.PlayerEnteredTrigger += ActivateButton;
+    }
+    private void OnDisable()
+    {
+        RayDoorTrigger.PlayerEnteredTrigger -= ActivateButton;
+    }
+
+    private void ActivateButton()
+    {
+        upButton.enabled = true;
+    }
+}
---
+++ Assets/Scripts/Ray/RayDoorTrigger.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayDoorTrigger : MonoBehaviour
+{
+    public static event Action PlayerEnteredTrigger;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (PlayerEnteredTrigger != null) PlayerEnteredTrigger.Invoke();
+    }
+}
