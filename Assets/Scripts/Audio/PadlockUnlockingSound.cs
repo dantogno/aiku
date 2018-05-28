b9e36dacf128fb3ec6dc70f914cc874d473b9097
danewheaton <wheaton.dane@gmail.com>
2018-05-28 12:29:44 -0500

    adds/implements unlock audio to the hub padlock, changed cursor texture and settings to better reflect default mouse cursor size/position

---
+++ Assets/Scripts/Audio/PadlockUnlockingSound.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// This class plays a sound when the hub padlock is unlocked.
+/// The script is applied to a child of the padlock.
+/// </summary>
+
+public class PadlockUnlockingSound : MonoBehaviour
+{
+    [SerializeField]
+    private LockInteract padlock;
+
+    private void OnEnable()
+    {
+        padlock.Unlocked += PlaySound;
+    }
+    private void OnDisable()
+    {
+        padlock.Unlocked -= PlaySound;
+    }
+
+    private void PlaySound()
+    {
+        AudioSource source = GetComponent<AudioSource>();
+
+        if (source != null) source.Play();
+    }
+}
